dcb4215478bac90548c4539d09b84a7c8e680884
Catherine <catherine.matta22399@gmail.com>
2020-12-01 16:41:02 -0500

    New Changes

--- app.py
+++ app.py
@@ -24,18 +24,19 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-text = ["Что","You know what? Im just going to fucking kill myself. Thats what I'm going to fucking do. I am going to fucking blow my fucking brains out. I'm going to kill myself. I'm going to kill myself and it's going to be your fucking fault. You're a piece of shit, Diar. You're a fucking piece of garbage.","Diar you really got me this time, you win brother. You literally want to trigger my anxiety and depression and you are going to make me have a panic attack in front of my computer. You really are a selfish asshole that doesn't care about anyone but yourself. Stop trying to bait me into killing myself. You just want to see me die at this point by any means necessary. fuck you Diar you are not going to make me kill myself."]
+
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 
 def start(update, context):
 	"""Send a message when the command /start is issued."""
 	update.message.reply_text("Feeling like an indecisive couch potato? Worry not and use Botato to help you make a decision! Write /help for more info.\n")
-	update.message.reply_text("Commands: \n/help --> how to use food bot.\n/diar --> roasts diar.\n/chuck --> returns Chuck Norris joke.\n/dog --> sends a random dog pic or gif.\n/meme --> sends the dankest memes.\n/maymay or /meemee --> finest collection of boomer hoomer.\n/cat --> praise the almighty inventor and get rewarded for it!\nThank.")
+	update.message.reply_text("Commands: \n/help --> how to use food bot.\n/chuck --> returns Chuck Norris joke.\n/dog --> sends a random dog pic or gif.\n/meme --> sends the dankest memes.\n/maymay or /meemee --> finest collection of boomer hoomer.\n/cat --> praise the almighty inventor and get rewarded for it!\nThank.")
 
 
 def help(update, context):
 	"""Send a message when the command /help is issued."""
+	update.message.reply_text("UPDATE: Feature currently broken.")
 	update.message.reply_text('Enter in thise order separated by a comma and space: Zip code or location. Resturant or food type. Number of choices.\nIf you dont want to specify a food type just put a blank space there or a 0 and it will default to just food places.\n')
 	update.message.reply_text('Example1: /food Central park, thai, 4 --> would give 4 nearest thai places by Central park\nExample2: /food CCNY --> would give 5 general food places by CCNY\nExample3: /food CCNY, pizza --> would give 5 pizza places by CCNY\nExample4: /food CCNY, 0, 10 --> would give the nearest 10 food places by CCNY\nExample5: /food CCNY, , 10 --> would give the nearest 10 food places by CCNY')
 	update.message.reply_text("Enjoy!")
@@ -50,9 +51,10 @@ def give_food(update,context):
 			number = 5
 
 	update.message.reply_text("\nHere are {size} {food_choice} places near: {user_input}".format(size=number,food_choice=food_choice,user_input=place))
-	location = googleapi.get_location(str(place))
-	results = googleapi.top_five(location,food_choice,int(number))
-	update.message.reply_text(results)
+	# location = googleapi.get_location(str(place))
+	# results = googleapi.top_five(location,food_choice,int(number))
+	# update.message.reply_text(results)
+	update.message.reply_text("Feature unavailable, must enable Google billing :(")
 	
 def cat(update,context):
 	update.message.reply_text("Hell yeah")
@@ -80,10 +82,7 @@ def maymay(update,context): #boomermeems
 	url = contents['url']
 	update.message.reply_text(url)
 
-def diar(update,context): #just for diar
-	num = secrets.randbelow(3)
-	update.message.reply_text("@DiarSanakov")
-	update.message.reply_text(text[num])
+
 
 def error(update, context):
 	"""Log Errors caused by Updates."""
@@ -118,7 +117,6 @@ def main():
 	dp.add_handler(CommandHandler("meemees", maymay))
 
 	#limited edition
-	dp.add_handler(CommandHandler("diar", diar)) 
 	dp.add_handler(CommandHandler("cat", cat))
 	dp.add_handler(CommandHandler("dog", dog))
 	dp.add_handler(CommandHandler("chuck",chuck))
@@ -127,9 +125,9 @@ def main():
 	dp.add_error_handler(error)
 
 	# Start the Bot
-	#updater.start_polling()
-	updater.start_webhook(listen="0.0.0.0",port=int(PORT),url_path=TOKEN)
-	updater.bot.setWebhook('https://rambotato.herokuapp.com/' + TOKEN)
+	updater.start_polling()
+	#updater.start_webhook(listen="0.0.0.0",port=int(PORT),url_path=TOKEN)
+	#updater.bot.setWebhook('https://rambotato.herokuapp.com/' + TOKEN)
 	# Run the bot until you press Ctrl-C or the process receives SIGINT,
 	# SIGTERM or SIGABRT. This should be used most of the time, since
 	# start_polling() is non-blocking and will stop the bot gracefully.
@@ -139,4 +137,4 @@ def main():
 if __name__ == '__main__':
 	main()
 	#port = int(os.environ.get('PORT', 5000))
-	#app.run()
+	app.run()
